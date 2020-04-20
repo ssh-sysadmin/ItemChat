17213da6dc02c0f7c0e25d2b3399a5377ac8f2c4
notyou12 <matanslevy6@gmail.com>
2020-04-20 16:40:47 -0400

    Made the damn thing work

--- src/main/java/tech/sshdoes/itemchat/ItemChat.java
+++ src/main/java/tech/sshdoes/itemchat/ItemChat.java
@@ -10,12 +10,13 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.lang.annotation.Annotation;;
+import java.util.Objects;
 
-public final class ItemChat extends JavaPlugin implements EventHandler, Listener {
+
+public final class ItemChat extends JavaPlugin implements Listener {
+
 
     @Override
     public void onEnable() {
@@ -24,36 +25,22 @@ public final class ItemChat extends JavaPlugin implements EventHandler, Listener
         // Plugin startup message - Plugin made by SSH#4388
 
     }
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    @SuppressWarnings("deprecation")
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onChatSend(AsyncPlayerChatEvent e){
         Player player = e.getPlayer();
         String message = e.getMessage();
-        ItemMeta item = player.getItemInHand().getItemMeta();
+        ItemStack item = player.getItemInHand();
         //TextComponent header = new TextComponent("");
-        if(message.equals("[i]")) {
-            TextComponent message2 = new TextComponent(item.getDisplayName());
-            message2.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(item.getEnchants().toString()).create()));
+        if(message.equalsIgnoreCase("[i]")) {
+            TextComponent tc = new TextComponent(Objects.requireNonNull(item.getItemMeta()).getDisplayName());
+            TextComponent hovermessage = new TextComponent((net.md_5.bungee.api.chat.BaseComponent) item.getItemMeta().getLore());
+            tc.setHoverEvent(new HoverEvent(net.md_5.bungee.api.chat.HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(hovermessage).create()));
             //header.addExtra(message2);
             //TextComponent finalmessage = header;
             e.setCancelled(true);
-
-            getServer().spigot().broadcast(message2);
+            player.chat(tc.toLegacyText());
 
         }
-    }
-
-    @Override
-    public EventPriority priority() {
-        return null;
-    }
-
-    @Override
-    public boolean ignoreCancelled() {
-        return false;
-    }
-
-    @Override
-    public Class<? extends Annotation> annotationType() {
-        return null;
     }
 }
