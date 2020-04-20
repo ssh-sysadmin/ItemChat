27c09d1e93487e9967f53d2b6226f07b4367cabc
notyou12 <matanslevy6@gmail.com>
2020-04-20 13:46:31 -0400

    Lots of updates

--- src/main/java/tech/sshdoes/itemchat/ItemChat.java
+++ src/main/java/tech/sshdoes/itemchat/ItemChat.java
@@ -1,5 +1,8 @@
 package tech.sshdoes.itemchat;
 
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -7,10 +10,10 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-import tech.sshdoes.itemchat.commands.i;
 
-import java.lang.annotation.Annotation;
+import java.lang.annotation.Annotation;;
 
 public final class ItemChat extends JavaPlugin implements EventHandler, Listener {
 
@@ -18,17 +21,24 @@ public final class ItemChat extends JavaPlugin implements EventHandler, Listener
     public void onEnable() {
         getServer().getConsoleSender().sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "ItemChat has loaded. Thanks for downloading (https://github.com/ssh-sysadmin/itemchat)");
         getServer().getPluginManager().registerEvents(this, this);
-        getCommand("i").setExecutor(new i());
         // Plugin startup message - Plugin made by SSH#4388
 
     }
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onChatSend(AsyncPlayerChatEvent e){
         Player player = e.getPlayer();
         String message = e.getMessage();
-        ItemStack item = player.getItemInHand();
-        if(message.equals("[i]")){
-            player.chat(item.toString());
+        ItemMeta item = player.getItemInHand().getItemMeta();
+        //TextComponent header = new TextComponent("");
+        if(message.equals("[i]")) {
+            TextComponent message2 = new TextComponent(item.getDisplayName());
+            message2.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(item.getEnchants().toString()).create()));
+            //header.addExtra(message2);
+            //TextComponent finalmessage = header;
+            e.setCancelled(true);
+
+            getServer().spigot().broadcast(message2);
+
         }
     }
 
